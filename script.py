f2e7c3fb140ee7ca12dcbdd507a293c89cb6044c
Mati658 <matiasaltamira@outlook.com>
2026-03-12 15:38:42 -0300

    Cambio de api para la tabla nacional de zona A y B

--- script.py
+++ script.py
@@ -1,18 +1,45 @@
+from pytest import fixture
 import requests
 from supabaseService import Supabase
 
 def main():
     supabase = Supabase()
-    url_liga = 'https://api.promiedos.com.ar/league/tables_and_fixtures/ebj'
+    url_liga = 'https://webws.365scores.com/web/standings/?appTypeId=5&langId=14&timezoneName=America/Buenos_Aires&userCountryId=382&competitions=419&live=false&withSeasonsFilter=true'
     response = requests.get(url_liga)
 
     if response.status_code == 200:
         data = response.json()
         if data:
-            fixture = {
-                'zona_a': data['tables_groups'][0]['tables'][0]['table']['rows'],
-                'zona_b': data['tables_groups'][0]['tables'][1]['table']['rows']
-            }
+            fixture = {'zona_a': [], 'zona_b': []}
+
+            for equipo in data['standings'][0]['rows']:
+                
+                if equipo['groupNum'] == 1:
+                    agregar = {
+                        'name': equipo['competitor']['name'],
+                        'num': equipo['position'],
+                        'points': equipo['points'],
+                        'played': equipo['gamePlayed'],
+                        'gfga': f"{equipo['for']} : {equipo['against']}",
+                        'ratio': equipo['ratio'],
+                        'wons': equipo['gamesWon'],
+                        'even': equipo['gamesEven'],
+                        'losts': equipo['gamesLost'],
+                    }
+                    fixture['zona_a'].append(agregar)
+                else:
+                    agregar = {
+                        'name': equipo['competitor']['name'],
+                        'num': equipo['position'],
+                        'points': equipo['points'],
+                        'played': equipo['gamePlayed'],
+                        'gfga': f"{equipo['for']} : {equipo['against']}",
+                        'ratio': equipo['ratio'],
+                        'wons': equipo['gamesWon'],
+                        'even': equipo['gamesEven'],
+                        'losts': equipo['gamesLost'],
+                    }
+                    fixture['zona_b'].append(agregar)
 
             supabase.upload_data('tabla_nacional', fixture)
         else:
